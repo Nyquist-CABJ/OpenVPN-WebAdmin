86dd79a85deb28de5d6bdcd1d5019eb1603abb55
Nyquist-CABJ <96484684+Nyquist-CABJ@users.noreply.github.com>
2026-07-16 20:25:22 -0300

    Update db.example.php

--- web/app/Config/db.example.php
+++ web/app/Config/db.example.php
@@ -1,10 +1,11 @@
 <?php
-$db_host = 'localhost';
-$db_name = 'openvpn';
-$db_user = 'openvpn_db';
-$db_pass = 'AlternativaGratisAR';
+$host = 'localhost';
+$dbname = 'openvpn';
+$user = 'openvpn_db';
+$pass = 'AlternativaGratisAR';
+
 try {
-    $pdo = new PDO("mysql:host=$db_host;db_name=$db_name;charset=utf8", $db_user, $db_pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión a la base de datos.");
